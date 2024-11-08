05dd355086db57a20974c12ab5ffdf4bfed1cef3
Aryan Goyal <aryangoyal90@outlook.com>
2024-11-09 01:09:17 +0530

    Update Riddles.js

--- src/components/Riddles.js
+++ src/components/Riddles.js
@@ -1,126 +1,124 @@
-const Data = [
+[
   {
     "riddle": 1,
-    "question": "Starting at zero, I grow quite slow, But give me time, and I’ll overflow. My sequence is famous, my growth is fine, And the golden ratio is mine. Add my last two numbers to make me soar.",
+    "question": "Starting at zero, I grow quite slow, But give me time, and I\u2019ll overflow. My sequence is famous, my growth is fine, And the golden ratio is mine. Add my last two numbers to make me soar.",
     "answer": "Fibonacci",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest1-1731087332"
   },
   {
     "riddle": 2,
-    "question": "I run forever if you don’t give me a limit, With each step, I refine and commit. I work in O(log n), but my start is near, I dive into binary, my purpose is clear. I break into halves, but I’m never content.",
+    "question": "I run forever if you don\u2019t give me a limit, With each step, I refine and commit. I work in O(log n), but my start is near, I dive into binary, my purpose is clear. I break into halves, but I\u2019m never content.",
     "answer": "Binary Search",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/code-wars-planetry-quest"
   },
   {
     "riddle": 3,
-    "question": "I'm a data structure, flexible yet small, My “LIFO” policy is known to all. Push and pop, I work just right, With one way in and out, nice and tight. Who am I?",
+    "question": "I'm a data structure, flexible yet small, My \u201cLIFO\u201d policy is known to all. Push and pop, I work just right, With one way in and out, nice and tight. Who am I?",
     "answer": "Stack",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest2-1"
   },
   {
     "riddle": 4,
-    "question": "I’m all about truth, with no room for lies, A yes or no, no compromise. In code, I’m a choice. What am I?",
+    "question": "I\u2019m all about truth, with no room for lies, A yes or no, no compromise. In code, I\u2019m a choice. What am I?",
     "answer": "Boolean",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest2-1"
   },
   {
     "riddle": 5,
     "question": "I'm used for sorting, in code I dance, Divide and conquer is my chance. Splitting arrays is my game. What am I?",
     "answer": "Merge Sort",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/code-wars-star-quest"
   },
   {
     "riddle": 6,
-    "question": "If you want unique values without a fuss, I’m the data structure you can trust. Duplicates I won't entertain, Collecting distinct items is my domain. What am I?",
+    "question": "If you want unique values without a fuss, I\u2019m the data structure you can trust. Duplicates I won't entertain, Collecting distinct items is my domain. What am I?",
     "answer": "Set",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest1-1731087332"
   },
   {
     "riddle": 7,
-    "question": "I’m a safety measure that is utilized to protect against unauthorized access to a computer system. What am I?",
+    "question": "I\u2019m a safety measure that is utilized to protect against unauthorized access to a computer system. What am I?",
     "answer": "Firewall",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest2-1"
   },
   {
     "riddle": 8,
-    "question": "I am an electronic device that can do complex numbers. I can be held in your hands, and I’m synonymous with computers. What am I?",
+    "question": "I am an electronic device that can do complex numbers. I can be held in your hands, and I\u2019m synonymous with computers. What am I?",
     "answer": "Calculator",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest2-1"
   },
   {
     "riddle": 9,
     "question": "I hold values that change as you go; without me, your program would be slow. What am I?",
     "answer": "Variable",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest2-1"
   },
   {
     "riddle": 10,
     "question": "I keep track of every change, in your code, nothing's out of range. I help you collaborate and never lose your way. What am I?",
     "answer": "Git/GitHub",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest1-1731087332"
   },
   {
     "riddle": 11,
-    "question": "I’m a function that calls itself to solve a task, With every call, I shrink the problem fast. But if you forget the base case, beware! I’ll spiral out of control and fill the stack with despair. What am I?",
+    "question": "I\u2019m a function that calls itself to solve a task, With every call, I shrink the problem fast. But if you forget the base case, beware! I\u2019ll spiral out of control and fill the stack with despair. What am I?",
     "answer": "Recursion",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest2-1"
   },
   {
     "riddle": 12,
-    "question": "I store your work, making sure it’s never gone. You commit to me, and I’ll keep it safe from now on. What am I?",
+    "question": "I store your work, making sure it\u2019s never gone. You commit to me, and I\u2019ll keep it safe from now on. What am I?",
     "answer": "A repository/GitHub",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest1-1731087332"
   },
   {
     "riddle": 13,
-    "question": "Teachers love me, students sigh, Clunky and slow, I’m not the sly. Basics are my claim to fame, In IDEs, I’m last in the game. What am I?",
+    "question": "Teachers love me, students sigh, Clunky and slow, I\u2019m not the sly. Basics are my claim to fame, In IDEs, I\u2019m last in the game. What am I?",
     "answer": "Code::Blocks",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest1-1731087332"
   },
   {
     "riddle": 14,
-    "question": "“Is it too late now to say SORRY?”… If you could code an apology in ASCII, what would it look like?",
-    "hint": "Convert the word “SORRY” into ASCII decimal codes.",
+    "question": "\u201cIs it too late now to say SORRY?\u201d\u2026 If you could code an apology in ASCII, what would it look like?",
+    "hint": "Convert the word \u201cSORRY\u201d into ASCII decimal codes.",
     "answer": "Song: 'Sorry' ASCII for 'SORRY': 83 79 82 82 89 Final answer: 8307982789",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest1-1731087332"
   },
   {
     "riddle": 15,
-    "question": "“I got a blank space, baby…” If that blank space were in hexadecimal, what would it be?",
+    "question": "\u201cI got a blank space, baby\u2026\u201d If that blank space were in hexadecimal, what would it be?",
     "hint": "Hex code for white space and blank elements &nbsp; #ffffff 0x20 20",
     "answer": "#ffffff, 0x20, or 20",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/code-wars-planetry-quest"
   },
   {
     "riddle": 16,
-    "question": "I’m a place where data waits in line, FIFO is my rule, I’m organized just fine. From head to tail, I move with grace, I’m the best when order’s a race. What am I?",
+    "question": "I\u2019m a place where data waits in line, FIFO is my rule, I\u2019m organized just fine. From head to tail, I move with grace, I\u2019m the best when order\u2019s a race. What am I?",
     "answer": "Queue",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest2-1"
   },
   {
     "riddle": 17,
-    "question": "I may be null, I may be not, I point to memory, no matter the spot. In code, I reference, but don’t always show, When I’m undefined, your code might throw. What am I?",
+    "question": "I may be null, I may be not, I point to memory, no matter the spot. In code, I reference, but don\u2019t always show, When I\u2019m undefined, your code might throw. What am I?",
     "answer": "Pointer",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest1-1731087332"
   },
   {
     "riddle": 18,
-    "question": "I’m often sorted or searched through, In computer science, I help you. Whether ascending or descending, I keep data in order, never ending. What am I?",
+    "question": "I\u2019m often sorted or searched through, In computer science, I help you. Whether ascending or descending, I keep data in order, never ending. What am I?",
     "answer": "Array",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest1-1731087332"
   },
   {
     "riddle": 19,
     "question": "I break your code, yet help it run, With step-by-step, I get things done. A process tracked, start to end, To find the bug, I am your friend. What am I?",
     "answer": "Debugging",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest2-1"
   },
   {
     "riddle": 20,
-    "question": "I’m a chunk of code, I have my place, Reusable and neat, in any case. With input and output, I work with care, Call me when needed, I’ll always be there. What am I?",
+    "question": "I\u2019m a chunk of code, I have my place, Reusable and neat, in any case. With input and output, I work with care, Call me when needed, I\u2019ll always be there. What am I?",
     "answer": "Function",
-    "redirectLink": "https://google.com"
+    "redirectLink": "https://www.hackerrank.com/codequest2-1"
   }
 ]
-
-export default Data;
