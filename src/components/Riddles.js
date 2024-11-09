33f9b62c51ad79103b73b8b5006678c1d4431a4c
AryanAg08 <aryangoyal90@outlook.com>
2024-11-09 15:10:26 +0530

    Adding

--- src/components/Riddles.js
+++ src/components/Riddles.js
@@ -9,25 +9,25 @@ const Data = [
     "riddle": 2,
     "question": "I run forever if you don\u2019t give me a limit, With each step, I refine and commit. I work in O(log n), but my start is near, I dive into binary, my purpose is clear. I break into halves, but I\u2019m never content.",
     "answer": "Binary Search",
-    "redirectLink": "https://www.hackerrank.com/code-wars-planetry-quest"
+    "redirectLink": "https://www.hackerrank.com/code-quest-3-1731136144"
   },
   {
     "riddle": 3,
     "question": "I'm a data structure, flexible yet small, My \u201cLIFO\u201d policy is known to all. Push and pop, I work just right, With one way in and out, nice and tight. Who am I?",
     "answer": "Stack",
-    "redirectLink": "https://www.hackerrank.com/codequest2-1"
+    "redirectLink": "https://www.hackerrank.com/planetry-conquest"
   },
   {
     "riddle": 4,
     "question": "I\u2019m all about truth, with no room for lies, A yes or no, no compromise. In code, I\u2019m a choice. What am I?",
     "answer": "Boolean",
-    "redirectLink": "https://www.hackerrank.com/codequest2-1"
+    "redirectLink": "https://www.hackerrank.com/planetry-conquest"
   },
   {
     "riddle": 5,
     "question": "I'm used for sorting, in code I dance, Divide and conquer is my chance. Splitting arrays is my game. What am I?",
     "answer": "Merge Sort",
-    "redirectLink": "https://www.hackerrank.com/code-wars-star-quest"
+    "redirectLink": "https://www.hackerrank.com/code-quest-3-1731136144"
   },
   {
     "riddle": 6,
@@ -39,19 +39,19 @@ const Data = [
     "riddle": 7,
     "question": "I\u2019m a safety measure that is utilized to protect against unauthorized access to a computer system. What am I?",
     "answer": "Firewall",
-    "redirectLink": "https://www.hackerrank.com/codequest2-1"
+    "redirectLink": "https://www.hackerrank.com/planetry-conquest"
   },
   {
     "riddle": 8,
     "question": "I am an electronic device that can do complex numbers. I can be held in your hands, and I\u2019m synonymous with computers. What am I?",
     "answer": "Calculator",
-    "redirectLink": "https://www.hackerrank.com/codequest2-1"
+    "redirectLink": "https://www.hackerrank.com/planetry-conquest"
   },
   {
     "riddle": 9,
     "question": "I hold values that change as you go; without me, your program would be slow. What am I?",
     "answer": "Variable",
-    "redirectLink": "https://www.hackerrank.com/codequest2-1"
+    "redirectLink": "https://www.hackerrank.com/planetry-conquest"
   },
   {
     "riddle": 10,
@@ -63,7 +63,7 @@ const Data = [
     "riddle": 11,
     "question": "I\u2019m a function that calls itself to solve a task, With every call, I shrink the problem fast. But if you forget the base case, beware! I\u2019ll spiral out of control and fill the stack with despair. What am I?",
     "answer": "Recursion",
-    "redirectLink": "https://www.hackerrank.com/codequest2-1"
+    "redirectLink": "https://www.hackerrank.com/planetry-conquest"
   },
   {
     "riddle": 12,
@@ -89,13 +89,13 @@ const Data = [
     "question": "\u201cI got a blank space, baby\u2026\u201d If that blank space were in hexadecimal, what would it be?",
     "hint": "Hex code for white space and blank elements &nbsp; #ffffff 0x20 20",
     "answer": "#ffffff, 0x20, or 20",
-    "redirectLink": "https://www.hackerrank.com/code-wars-planetry-quest"
+    "redirectLink": "https://www.hackerrank.com/code-quest-3-1731136144"
   },
   {
     "riddle": 16,
     "question": "I\u2019m a place where data waits in line, FIFO is my rule, I\u2019m organized just fine. From head to tail, I move with grace, I\u2019m the best when order\u2019s a race. What am I?",
     "answer": "Queue",
-    "redirectLink": "https://www.hackerrank.com/codequest2-1"
+    "redirectLink": "https://www.hackerrank.com/planetry-conquest"
   },
   {
     "riddle": 17,
@@ -113,13 +113,13 @@ const Data = [
     "riddle": 19,
     "question": "I break your code, yet help it run, With step-by-step, I get things done. A process tracked, start to end, To find the bug, I am your friend. What am I?",
     "answer": "Debugging",
-    "redirectLink": "https://www.hackerrank.com/codequest2-1"
+    "redirectLink": "https://www.hackerrank.com/planetry-conquest"
   },
   {
     "riddle": 20,
     "question": "I\u2019m a chunk of code, I have my place, Reusable and neat, in any case. With input and output, I work with care, Call me when needed, I\u2019ll always be there. What am I?",
     "answer": "Function",
-    "redirectLink": "https://www.hackerrank.com/codequest2-1"
+    "redirectLink": "https://www.hackerrank.com/planetry-conquest"
   }
 ]
 
